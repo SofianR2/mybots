53a63ca71ab2afa5260c0be07b6439286c044ed0
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-09 02:53:18 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -8,5 +8,5 @@ x = 0
 y = 0
 z = 0.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x,y + 1,z + 1] , size=[length,width,height])
 pyrosim.End()
