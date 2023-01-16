f7bd09b57a9662eaa7d61eb922bfe5f7c2b917a0
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 14:05:37 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -27,14 +27,14 @@ for i in range(1000):
   bodyIndex = robotId,
   jointName = "Torso_BackLeg",
   controlMode = p.POSITION_CONTROL,
-  targetPosition = random.randrange(-1, 1) * 0.3,
+  targetPosition = random.randrange(-1, 1) * 0.25,
   maxForce = 500)
   
   pyrosim.Set_Motor_For_Joint(
   bodyIndex = robotId,
   jointName = "Torso_FrontLeg",
   controlMode = p.POSITION_CONTROL,
-  targetPosition = random.randrange(-1, 1) * 0.3,
+  targetPosition = random.randrange(-1, 1) * 0.25,
   maxForce = 500)
   
   time.sleep(1/60)
