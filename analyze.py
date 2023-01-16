32889537709e6917bd99876e10bdf93b6528ac0d
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 15:39:51 -0600

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -11,6 +11,6 @@ targetAngles = numpy.load('data/targetAngles.npy')
 
 #matplotlib.pyplot.plot(backLegSensorValues, linewidth=5, label='Back Leg')
 #matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
-matplotlib.pyplot.plot(numpy.sin/4 * targetAngles, numpy.sin/4 * numpy.sin(targetAngles))
+matplotlib.pyplot.plot(targetAngles, numpy.sin(targetAngles))
 #matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
