1ab469492969539db7ba67a1193faccaad149dea
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 16:29:09 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -6,9 +6,13 @@ import numpy
 import math
 import random
 
-amplitude = numpy.pi/4
-frequency = 10
-phaseOffset = 0
+backAmplitude = numpy.pi/4
+backFrequency = 10
+backPhaseOffset = 0
+
+frontAmplitude = numpy.pi/4
+frontFrequency = 10
+frontPhaseOffset = 0
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -25,8 +29,11 @@ targetAngles = numpy.zeros(1000)
 #targetAngles = numpy.linspace(0, 2 * numpy.pi, 1000)
 #targetAngles = numpy.sin(targetAngles) * numpy.pi/4
 
-targetAngles = numpy.linspace(phaseOffset, 2 * numpy.pi * frequency, 1000)
-targetAngles = numpy.sin(targetAngles) * amplitude
+backTargetAngles = numpy.linspace(backPhaseOffset, 2 * numpy.pi * backFrequency, 1000)
+backTargetAngles = numpy.sin(backTargetAngles) * backAmplitude
+
+frontTargetAngles = numpy.linspace(frontPhaseOffset, 2 * numpy.pi * frontFrequency, 1000)
+frontTargetAngles = numpy.sin(frontTargetAngles) * frontAmplitude
 
 #numpy.save('data/targetAngles', targetAngles)
 #exit()
