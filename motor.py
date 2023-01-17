587104a922eeaead1a3f20c426cec385a15e6cf0
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 22:47:01 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,3 +1,16 @@
+import pybullet as p
+import time
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import math
+import random
+import constants as c
+
 class MOTOR:
   def __init__(self, jointName):
-    self.jointName = jointName
+    self.jointName = jointName'
+    
+  def Prepare_To_Act(self):
+    for jointName in pyrosim.jointNamesToIndices:
+      self.motor[jointName] = MOTOR(jointName)
