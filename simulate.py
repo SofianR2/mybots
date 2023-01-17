51cf70ff94046f085028f89bc687cde400589db1
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-17 01:15:37 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -28,11 +28,11 @@ targetAngles = numpy.zeros(1000)
 #targetAngles = numpy.linspace(0, 2 * numpy.pi, 1000)
 #targetAngles = numpy.sin(targetAngles) * numpy.pi/4
 
-backTargetAngles = numpy.linspace(c.backPhaseOffset, 2 * numpy.pi * c.backFrequency, 1000)
-backTargetAngles = numpy.sin(backTargetAngles) * c.backAmplitude
+#backTargetAngles = numpy.linspace(c.backPhaseOffset, 2 * numpy.pi * c.backFrequency, 1000)
+#backTargetAngles = numpy.sin(backTargetAngles) * c.backAmplitude
 
-frontTargetAngles = numpy.linspace(c.frontPhaseOffset, 2 * numpy.pi * c.frontFrequency, 1000)
-frontTargetAngles = numpy.sin(frontTargetAngles) * c.frontAmplitude
+#frontTargetAngles = numpy.linspace(c.frontPhaseOffset, 2 * numpy.pi * c.frontFrequency, 1000)
+#frontTargetAngles = numpy.sin(frontTargetAngles) * c.frontAmplitude
 
 #numpy.save('data/targetAngles', targetAngles)
 #exit()
