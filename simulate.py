428851dddb0fc5086a8d4736d9302e8f2ac551e6
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 20:27:10 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -16,9 +16,9 @@ import constants as c
 #p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 #p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
+#planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
-p.loadSDF("world.sdf")
+#p.loadSDF("world.sdf")
 #pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
