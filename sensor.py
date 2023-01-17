18a8129e7388445e8a786bd3cd57cd45ba00c1bd
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 22:29:38 -0600

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -12,3 +12,7 @@ class SENSOR:
     self.linkName = linkName
     self.values = numpy.zeros(1000)
     print(self.values)
+    
+  def Get_Value(self, i):
+    self.values[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+    print(self.values)
