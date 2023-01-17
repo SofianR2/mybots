7b5ee29f685d5a3ba86fffc3e950f34efa8287f9
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 23:07:18 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -16,18 +16,9 @@ class MOTOR:
     self.amplitude = c.backAmplitude
     self.frequency = c.backFrequency
     self.offset = c.backPhaseOffset
-    '''
-    pyrosim.Set_Motor_For_Joint(
-      bodyIndex = self.robotId,
-      jointName = "Torso_BackLeg",
-      controlMode = p.POSITION_CONTROL,
-      targetPosition = backTargetAngles[i],
-      maxForce = 500)
 
-      pyrosim.Set_Motor_For_Joint(
-      bodyIndex = self.robotId,
-      jointName = "Torso_FrontLeg",
-      controlMode = p.POSITION_CONTROL,
-      targetPosition = frontTargetAngles[i],
-      maxForce = 500)
-    '''
+    backTargetAngles = numpy.linspace(self.offset, 2 * numpy.pi * self.frequency, 1000)
+    backTargetAngles = numpy.sin(backTargetAngles) * self.amplitude
+
+    frontTargetAngles = numpy.linspace(self.offset, 2 * numpy.pi * self.frequency, 1000)
+    frontTargetAngles = numpy.sin(frontTargetAngles) * self.amplitude
