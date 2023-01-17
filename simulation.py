5fe2b6373f800432804ad0a8dc5f3eb64e767a48
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 20:59:21 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -13,12 +13,13 @@ from robot import ROBOT
 class SIMULATION:
   def __init__(self):
     self.physicsClient = p.connect(p.GUI)
-    self.robotId = p.loadURDF("body.urdf")
+    #self.robotId = p.loadURDF("body.urdf")
+    self.world = WORLD()
+    self.robot = ROBOT()    
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-9.8)
     pyrosim.Prepare_To_Simulate(self.robotId)
-    self.world = WORLD()
-    self.robot = ROBOT()    
+    
     
   def Run(self):
     backLegSensorValues = numpy.zeros(1000)
