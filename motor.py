e2d0d9c91e2879b643cad08bf7966dfad6208027
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 22:49:47 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -9,7 +9,8 @@ import constants as c
 
 class MOTOR:
   def __init__(self, jointName):
-    self.jointName = jointName'
+    self.jointName = jointName
+    Prepare_To_Act()
     
   def Prepare_To_Act(self):
     pass
