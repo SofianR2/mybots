7aa0eca9b60d1ee4d74908e6e462ea620a52626f
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 20:43:04 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -13,6 +13,7 @@ from robot import ROBOT
 class SIMULATION:
   def __init__(self):
     self.physicsClient = p.connect(p.GUI)
+    self.robotId = p.loadURDF("body.urdf")
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-9.8)
     pyrosim.Prepare_To_Simulate(robotId)
