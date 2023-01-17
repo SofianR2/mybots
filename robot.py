7b9c13eb3221ce451ff1d784e3c99b8860069c1e
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-16 23:52:54 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -27,7 +27,14 @@ class ROBOT:
     
   def Prepare_To_Act(self):
     for jointName in pyrosim.jointNamesToIndices:
-      self.motors[jointName] = MOTOR(jointName)
+      self.motors[jointName] = MOTOR(jointName)\
+      
+    self.amplitude = c.backAmplitude
+    self.frequency = c.backFrequency
+    self.offset = c.backPhaseOffset
+
+    backTargetAngles = numpy.linspace(self.offset, 2 * numpy.pi * self.frequency, 1000)
+    backTargetAngles = numpy.sin(backTargetAngles) * self.amplitude
       
   def Act(self):
     for i in self.motors:
