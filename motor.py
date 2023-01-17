0da6c138fd192df2f97dea8c69a89095ecc7f2c4
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-17 01:35:44 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -17,7 +17,7 @@ class MOTOR:
     else:
       self.frequency = c.frequency
     self.offset = c.phaseOffset
-    self.motorValues = numpy.linspace(self.offset, 2 * numpy.pi * self.frequency, 1000)
+    self.motorValues = numpy.linspace(self.phaseOffset, 2 * numpy.pi * self.frequency, 1000)
     self.motorValues = numpy.sin(self.motorValues) * self.amplitude
     
   '''
