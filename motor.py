3b8b3dfedf5660b7c503b9f5bf914ac28a7671ad
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-17 01:06:53 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -13,7 +13,7 @@ class MOTOR:
     def Prepare_To_Act(self):
       self.amplitude = c.amplitude
       if self.jointName == b'Torso_BackLeg':
-        self.frequency = c.frequency * 2
+        self.frequency = 0
       else:
         self.frequency = c.frequency
       self.offset = c.phaseOffset
