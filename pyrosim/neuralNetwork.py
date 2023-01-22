f5cdc4f112b4b1139f3b2974ad8f1301475c6c21
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-22 00:12:05 -0600

    Update neuralNetwork.py

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -30,7 +30,8 @@ class NEURAL_NETWORK:
         
     def Update(self):
         for i in self.neurons:
-            print(i)
+            if self.neurons[neuronName].Is_Sensor_Neuron():
+                pass
 
 # ---------------- Private methods --------------------------------------
 
