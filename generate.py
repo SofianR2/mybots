8baf05cf50f4c55b2ca6590f7f84e977d03dae63
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-22 14:17:22 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -31,7 +31,7 @@ def Generate_Brain():
   #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
   #pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
   #pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1.0 )
-  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1.0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 5.0 )
   pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = 5.0 )
   pyrosim.End()
   
