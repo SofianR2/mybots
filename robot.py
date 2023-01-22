3ab74441a3867e8de9a15f9ab0df376b35284257
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-22 01:52:05 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -37,10 +37,11 @@ class ROBOT:
       if self.nn.Is_Motor_Neuron(neuronName):
         jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
         desiredAngle = self.nn.Get_Value_Of(neuronName)
+        self.motors[jointName].Set_Value(self.robotId, desiredAngle)
         print(neuronName, jointName, desiredAngle)
       
-    for i in self.motors:
-      self.motors[i].Set_Value(self.robotId, t)
+    #for i in self.motors:
+    #  self.motors[i].Set_Value(self.robotId, t)
   
   def Think(self):
     self.nn.Update()
