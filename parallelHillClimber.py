bd968d5580906b802d0557a102f5cd1d15472a5b
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 17:46:33 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -32,6 +32,7 @@ class PARALLEL_HILL_CLIMBER:
   def Evolve(self):
     for i in range(0, c.populationSize):
       self.parents[i].Start_Simulation("GUI")
+      print("HELLOOOOOOOOOOOOOOOOOOOOOO")
     for j in range(0, c.populationSize):
       print("HELLOOOOOOOOOOOOOOOOOOOOOO")
       self.parents[i].Wait_For_Simulation_To_End()
