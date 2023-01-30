d5767ba62a74ae0d4c301972239ddd7ac0cb0d67
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 13:07:22 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -37,7 +37,7 @@ class SIMULATION:
     
   def Run(self):
     if(self.directOrGUI == "GUI"):
-      time.sleep()
+      time.sleep(1/60)
 
     #backLegSensorValues = numpy.zeros(1000)
     #frontLegSensorValues = numpy.zeros(1000)
