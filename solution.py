35e5d784deb29fa13bd4e6ef84b6790f1b55bd9f
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-29 22:09:47 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,9 +13,6 @@ class SOLUTION:
     y=0
     z=0.5
     
-  def Evaluate(self):
-    os.system("python3 simulate.py")
-  
   def Create_World(self):
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z] , size=[length,width,height])
@@ -37,15 +34,15 @@ class SOLUTION:
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -0.5 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -0.5 )
     for currentRow in [0, 1, 2]:
       for CurrentColumn in [0, 1]:
         pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn + 3 , weight = self.weights[currentRow][currentColumn])
     pyrosim.End()
     
+  def Evaluate(self):
     Create_World()
     Create_Body()
     Create_Brain()
+    os.system("python3 simulate.py")
+  
+
