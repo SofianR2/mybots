74c1e54d1bdf2648816a019f9848692098802cde
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-29 21:05:40 -0600

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 from hillclimber import HILLCLIMBER
 import os
 
-for x in range(1, 6):
-  os.system("python3 generate.py")
-  os.system("python3 simulate.py")
+#for x in range(1, 6):
+#  os.system("python3 generate.py")
+#  os.system("python3 simulate.py")
