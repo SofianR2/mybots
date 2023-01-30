e2c47208e0a76722b4e97782652fa8d07b370ce2
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-29 23:25:40 -0600

    Update hillclimber.py

--- hillclimber.py
+++ hillclimber.py
@@ -1,11 +1,29 @@
 from solution import SOLUTION
+import constants as c
 
 class HILLCLIMBER:
   def __init__(self):
     self.parent = SOLUTION()
+    
+  def Spawn(self):
+    pass
+  
+  def Mutate(self):
+    pass
+  
+  def Select(self):
+    pass
+  
+  def Evolve_For_One_Generation(self):
+    self.Spawn()
+    self.Mutate()
+    self.child.Evaluate()
+    self.Select()
     
   def Evolve(self):
     self.parent.Create_World()
     self.parent.Create_Body()
     self.parent.Create_Brain()
     self.parent.Evaluate()
+    for currentGeneration in range(c.numberOfGenerations):
+      self.Evolve_For_One_Generation()
