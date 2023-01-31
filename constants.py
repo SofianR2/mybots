c6eabaff878b44bf9015944ded0bd407c6e21918
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 21:48:44 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import numpy
 amplitude = numpy.pi/8
 frequency = 10
 phaseOffset = 0
-numberOfGenerations = 1
-populationSize = 1
+numberOfGenerations = 5
+populationSize = 5
 numSensorNeurons = 9
 numMotorNeurons = 8
