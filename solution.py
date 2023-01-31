dd5fa8884ace41551f8c7a765f4cf46097757ecf
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 21:25:01 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -51,6 +51,8 @@ class SOLUTION:
     pyrosim.Send_Cube(name="RightLeg", pos=[x+0.5,y,z-0.5] , size=[length,width-0.8,height-0.8])
     pyrosim.Send_Joint(name = "FrontLeg_FrontLowerLeg" , parent= "FrontLeg" , child = "FrontLowerLeg" , type = "revolute", position = [x,y+1,z-0.5], jointAxis = "1 0 0")
     pyrosim.Send_Cube(name="FrontLowerLeg", pos=[x,y,z-1] , size=[length-0.8,width-0.8,height])
+    pyrosim.Send_Joint(name = "BackLeg_BackLowerLeg" , parent= "BackLeg" , child = "BackLowerLeg" , type = "revolute", position = [x,y-1,z-0.5], jointAxis = "1 0 0")
+    pyrosim.Send_Cube(name="BackLowerLeg", pos=[x,y,z] , size=[length-0.8,width-0.8,height])
     pyrosim.End()
 
   def Create_Brain(self):
@@ -61,12 +63,18 @@ class SOLUTION:
     pyrosim.Send_Sensor_Neuron(name = 3 , linkName = "LeftLeg")
     pyrosim.Send_Sensor_Neuron(name = 4 , linkName = "RightLeg")
     pyrosim.Send_Sensor_Neuron(name = 5 , linkName = "FrontLowerLeg")
+    pyrosim.Send_Sensor_Neuron(name = 6 , linkName = "BackLowerLeg")
+    pyrosim.Send_Sensor_Neuron(name = 7 , linkName = "RightLowerLeg")
+    pyrosim.Send_Sensor_Neuron(name = 8 , linkName = "LeftLowerLeg")
     
     pyrosim.Send_Motor_Neuron( name = 6 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 7 , jointName = "Torso_FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 8 , jointName = "Torso_LeftLeg")
     pyrosim.Send_Motor_Neuron( name = 9 , jointName = "Torso_RightLeg")
     pyrosim.Send_Motor_Neuron( name = 10 , jointName = "FrontLeg_FrontLowerLeg")
+    pyrosim.Send_Motor_Neuron( name = 11 , jointName = "BackLeg_BackLowerLeg")
+    pyrosim.Send_Motor_Neuron( name = 12 , jointName = "RightLeg_RightLowerLeg")
+    pyrosim.Send_Motor_Neuron( name = 13 , jointName = "LeftLeg_LeftLowerLeg")
     
     for currentRow in range(0, c.numSensorNeurons):
       for currentColumn in range(0, c.numMotorNeurons):
