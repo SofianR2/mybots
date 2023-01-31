235904ed06d58e19acca81779e747b64dc2cfffc
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 21:06:54 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -45,7 +45,7 @@ class SOLUTION:
     pyrosim.Send_Cube(name="BackLeg", pos=[x,y-0.5,z-0.5] , size=[length-0.8,width,height-0.8])
     pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [x,y+0.5,z+0.5], jointAxis = "1 0 0")
     pyrosim.Send_Cube(name="FrontLeg", pos=[x,y+0.5,z-0.5] , size=[length-0.8,width,height-0.8])
-    pyrosim.Send_Joint(name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = [x-0.5,y,z+0.5], jointAxis = "1 0 0")
+    pyrosim.Send_Joint(name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = [x-0.5,y,z+0.5], jointAxis = "0 1 0")
     pyrosim.Send_Cube(name="LeftLeg", pos=[x-0.5,y,z-0.5] , size=[length,width-0.8,height-0.8])
     pyrosim.End()
 
