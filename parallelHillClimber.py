793fcd026da0d1d21e9611c9129b44110f066f23
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 19:00:50 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -33,7 +33,7 @@ class PARALLEL_HILL_CLIMBER:
     self.children = {}
     
     
-    for i in self.parents.keys():      
+    for i in self.parents:      
       self.children[i] = copy.deepcopy(self.parents[i])
       self.children[i].Set_ID(self.nextAvailableID)
       self.nextAvailableID += 1
