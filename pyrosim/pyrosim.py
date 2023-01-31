c3513e76b402c9fbe7d9cf71f7b6e70ebce00761
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-01-30 20:52:44 -0600

    Update pyrosim.py

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -136,7 +136,7 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
 
 def Send_Joint(name,parent,child,type,position,jointAxis):
 
-    joint = JOINT(name,parent,child,type,position)
+    joint = JOINT(name,parent,child,type,position,jointAxis)
 
     joint.Save(f, jointAxis)
 
