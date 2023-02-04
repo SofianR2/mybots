431bcf940c44b40515af99a79d90eec7dafa062f
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-03 23:06:02 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -57,8 +57,9 @@ class ROBOT:
     #print(xCoordinateOfLinkZero)
     f = open("tmp" + str(self.solutionID) + ".txt", "w")
     f.write(str(xCoordinateOfLinkZero))
-    f.close()
     os.system("rename tmp" + str(self.solutionID) + ".txt " + "fitness" + str(self.solutionID) + ".txt")    
+
+    f.close()
 
 
     #os.system("del fitness" + str(self.solutionID) + ".txt")    
