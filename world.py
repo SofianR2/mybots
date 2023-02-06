f1c6391dacbd3c885d0524d948c06faabfbdd14c
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-06 17:34:55 -0600

    Update world.py

--- world.py
+++ world.py
@@ -1,7 +1,9 @@
 import pybullet as p
 
 class WORLD:
-  def __init__(self):
+  def __init__(self, solutionID):
     self.planeId = p.loadURDF("plane.urdf")
     p.loadSDF("world.sdf")
+    p.loadSDF("world" + str(solutionID) + ".sdf")
+    
 
