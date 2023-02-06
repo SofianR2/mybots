dd5bcfc7ef612e7aad22e330d9092480513bc24c
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-06 17:10:41 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -43,6 +43,7 @@ class SOLUTION:
     x = self.x
     y = self.y
     z = self.z
+    pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z] , size=[length+5,width,height+5])
     ##############pyrosim.Start_URDF("body.urdf")
     pyrosim.Start_URDF("body" + str(self.myID) + ".urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z+0.5] , size=[length,width,height])
