457ee337dfe57150a4675a1821e16815258ff44a
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-05 21:29:58 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -17,7 +17,7 @@ class ROBOT:
     #self.motors = {}    
     self.solutionID = solutionID
     ###################self.robotId = p.loadURDF("body.urdf")
-    self.robotId = p.loadURDF("body" + str(solutionID) ".urdf")
+    self.robotId = p.loadURDF("body" + str(solutionID) + ".urdf")
     os.system("del body" + str(solutionID) + ".urdf")
     self.nn = NEURAL_NETWORK("brain" + str(solutionID) + ".nndf")
     os.system("del brain" + str(solutionID) + ".nndf")
