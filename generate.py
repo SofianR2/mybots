85efe963c0bd02813e04af1eed57711d19708988
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-06 16:55:19 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -14,7 +14,7 @@ z=0.5
 
 def Create_World():
   pyrosim.Start_SDF("world.sdf")
-  pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z] , size=[length + 10,width + 2,height + 5])
+  pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z] , size=[length,width,height])
   pyrosim.End()
   
 def Generate_Body():
