498900abfab338545f3ef3854558d113d39daefc
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-06 16:14:51 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -59,11 +59,10 @@ class ROBOT:
     basePosition = basePositionAndOrientation[0]
     #xCoordinateOfLinkZero = positionOfLinkZero[0]
     xCoordinateOfLinkZero = basePosition[0]
-    #zCoordinateOfLinkZero = basePosition[3]
+    zCoordinateOfLinkZero = basePosition[2]
     #print(xCoordinateOfLinkZero)
-    print(basePosition)
     f = open("tmp" + str(self.solutionID) + ".txt", "w")
-    f.write(str(xCoordinateOfLinkZero))
+    f.write(str(zCoordinateOfLinkZero))
     f.close()
     
     os.system("rename tmp" + str(self.solutionID) + ".txt " + "fitness" + str(self.solutionID) + ".txt")    
