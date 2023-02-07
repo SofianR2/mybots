678aacab9b894f3cc545e976377006ad6de46a4a
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-07 15:32:02 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -52,9 +52,9 @@ class SOLUTION:
     pyrosim.Send_Cube(name="LeftLeg", pos=[x,y,z-1] , size=[length-0.6,width-0.6,height])
     
     pyrosim.Send_Joint(name = "RightLeg_RightLowerLeg" , parent= "RightLeg" , child = "RightLowerLeg" , type = "revolute", position = [x,y,z-1.5], jointAxis = "0 1 0")
-    pyrosim.Send_Cube(name="RightLowerLeg", pos=[x,y,z-1.5] , size=[length-0.6,width-0.6,height])
+    pyrosim.Send_Cube(name="RightLowerLeg", pos=[x,y,z-0.5] , size=[length-0.6,width-0.6,height])
     pyrosim.Send_Joint(name = "LeftLeg_LeftLowerLeg" , parent= "LeftLeg" , child = "LeftLowerLeg" , type = "revolute", position = [x,y,z-1.5], jointAxis = "0 1 0")
-    pyrosim.Send_Cube(name="LeftLowerLeg", pos=[x,y,z-1.5] , size=[length-0.6,width-0.6,height])
+    pyrosim.Send_Cube(name="LeftLowerLeg", pos=[x,y,z-0.5] , size=[length-0.6,width-0.6,height])
     #pyrosim.Send_Joint(name = "RightLowerLeg_RightShoe" , parent= "RightLowerLeg" , child = "RightShoe" , type = "revolute", position = [x,y,z-1.5], jointAxis = "0 1 0")
     #pyrosim.Send_Cube(name="RightShoe", pos=[x,y,z-0.5] , size=[length,width-0.6,height-0.9])
     #pyrosim.Send_Joint(name = "LeftLeg_LeftLowerLeg" , parent= "LeftLowerLeg" , child = "LeftShoe" , type = "revolute", position = [x,y,z-0.5], jointAxis = "0 1 0")
