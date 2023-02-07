5ae1e2d32529b44a32cfdb78064c9191f119b5fa
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-06 23:07:25 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -46,7 +46,7 @@ class SOLUTION:
     ##############pyrosim.Start_URDF("body.urdf")
     pyrosim.Start_URDF("body" + str(self.myID) + ".urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z+0.5+height_offset] , size=[length,width,height])
-    pyrosim.Send_Joint(name = "Torso_RightLeg" , parent= "Torso" , child = "RightLeg" , type = "revolute", position = [x+0.5,y,z-1+height_offset], jointAxis = "0 1 0")
+    pyrosim.Send_Joint(name = "Torso_RightLeg" , parent= "Torso" , child = "RightLeg" , type = "revolute", position = [x+0.5,y+0.5,z+0.5+height_offset], jointAxis = "0 1 0")
     pyrosim.Send_Cube(name="RightLeg", pos=[x,y,z] , size=[length-0.8,width-0.8,height+1])
     
     
