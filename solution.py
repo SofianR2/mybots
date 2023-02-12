f31d02ed13f6e41e58d211ea529b6197b6a3fe32
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-12 17:01:10 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -62,7 +62,7 @@ class SOLUTION:
       pyrosim.Send_Cube(name= str(i), pos=[x,y,z+height_offset] , size=[length, width, height])
       height_offset = -0.5
       if(i!=self.max-1):
-        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(width/2),y,z+height_offset], jointAxis = "0 1 0")    
+        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(width/2)+1,y,z+height_offset], jointAxis = "0 1 0")    
       
       '''
       pyrosim.Send_Cube(name= str(i), pos=[x,y,z+height_offset] , size=[length, width, height])
