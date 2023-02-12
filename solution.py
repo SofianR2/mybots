9135597203981c0394fea8f670163ac13d827f56
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-12 15:39:57 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ class SOLUTION:
     self.x=0
     self.y=0
     self.z=0.5
-    self.max = 1
+    self.max = 2
    
     
   def Create_World(self):
@@ -57,14 +57,14 @@ class SOLUTION:
       width = random.uniform(0.1, 2)
       height = random.uniform(0.1, 2)
       pyrosim.Send_Cube(name= str(i), pos=[x,y,z+height_offset] , size=[length, width, height])
-      if i != self.max:
-        pyrosim.Send_Joint(name = str(i) + "_" + str(i+1), parent= str(i) , child = str(i+1) , type = "revolute", position = [x+0.5,y,z+height_offset], jointAxis = "0 1 0")
-      '''
+      #if i != self.max:
+      #  pyrosim.Send_Joint(name = str(i) + "_" + str(i+1), parent= str(i) , child = str(i+1) , type = "revolute", position = [x+0.5,y,z+height_offset], jointAxis = "0 1 0")
+
       if(i!=0):
         previous = i - 1
         current = i
         pyrosim.Send_Joint(name = str(previous) + "_" + str(current), parent= str(previous) , child = str(current) , type = "revolute", position = [x+0.5,y,z+height_offset], jointAxis = "0 1 0")
-     '''
+
     pyrosim.End()
 
   def Create_Brain(self):
