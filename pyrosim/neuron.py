bddceb567c5cc8001d6b633b08e4971caa42dae9
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-13 01:07:55 -0600

    Update neuron.py

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -73,7 +73,7 @@ class NEURON:
         self.Set_Value(0)
         for i in synapses:
             if(self.Get_Name() == i[1]):
-                print(i)##################################################################################################################
+                print(synapses[i])##################################################################################################################
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[i].Get_Weight(), neurons[i[0]].Get_Value())
         self.Threshold()
                 
