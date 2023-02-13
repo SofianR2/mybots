6d3c3880ee9a29d9bc756d89ba60752010bdaca9
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-12 18:01:25 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -45,7 +45,7 @@ class SOLUTION:
     y = self.y
     z = self.z
     height_offset = 1
-    joint_offset = 0
+    joint_offset = 1
     width_offset = 0
     
     ##############pyrosim.Start_URDF("body.urdf")
@@ -65,10 +65,10 @@ class SOLUTION:
       nex = i + 1
       pyrosim.Send_Cube(name= str(i), pos=[x+width_offset,y,z+height_offset] , size=[length, width, height])
       if(i!=self.max-1):
-        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+length/2+joint_offset,y,z+height_offset], jointAxis = "0 1 0")
+        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+length/2*joint_offset,y,z+height_offset], jointAxis = "0 1 0")
         height_offset = -0.5
         width_offset = length/2
-        joint_offset = length/2
+        joint_offset = 2
         
 
       
