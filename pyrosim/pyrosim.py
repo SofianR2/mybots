18d738be1e910a5b5eb34775ddbfab3ff7a55746
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-12 19:28:28 -0600

    Update pyrosim.py

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -107,7 +107,7 @@ def Prepare_To_Simulate(bodyID):
     Prepare_Joint_Dictionary(bodyID)
 
 #def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
-def Send_Link(name, pos, size, objectType):
+def Send_Link(name, pos, size, objectType, color):
 
     global availableLinkIndex
 
@@ -123,7 +123,7 @@ def Send_Link(name, pos, size, objectType):
 
         links.append(link)
     else:
-        link = LINK_URDF(name,pos,size)
+        link = LINK_URDF(name,pos,size,color)
 
         links.append(link)
 
@@ -137,9 +137,9 @@ def Send_Link(name, pos, size, objectType):
 
     availableLinkIndex = availableLinkIndex + 1
     
-def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
+def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1], color):
 
-    Send_Link(name,pos,size,"box")
+    Send_Link(name,pos,size,"box", color)
 
 def Send_Sphere(name="default",pos=[0,0,0],size=[0.5]):
 
