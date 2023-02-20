9515bfc7f10083c7fcbff68413e6ec022a30b803
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 01:49:00 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -73,9 +73,9 @@ class SOLUTION:
         pyrosim.Send_Cube(name= str(i), pos=[x+(length/2*width_offset),y,z+height_offset] , size=[length, width, height], color='    <color rgba="0.0 200.0 0.0 1.0"/>', cname = '<material name="Green">')
         self.get_sensor.append(0)
       if(i!=self.max-1):
-        random_multiplier = numpy.random.rand(0, 1) * 2 - 1
+        #random_multiplier = numpy.random.rand(0, 1) * 2 - 1
         print(str(current) + " " + str(nex))
-        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(random_multiplier*(length/2*joint_offset)),y,z+height_offset], jointAxis = "0 1 0")
+        pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+((length/2*joint_offset)),y,z+height_offset], jointAxis = "0 1 0")
         height_offset = -0.5
         width_offset = 1
         joint_offset = 2
