5d6bfa6b8973ce9685a6a207b399c646e9e1d919
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 14:54:15 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -182,12 +182,12 @@ class SOLUTION:
     for i, link in enumerate(self.link_list):
       if(link.has_link == 1):
         pyrosim.Send_Sensor_Neuron(name = sensor_number, linkName = str(i))
-        print("name = " + str(sensor_number) + " linkName = " + str(i))
+        #print("name = " + str(sensor_number) + " linkName = " + str(i))
         sensor_number = sensor_number + 1
         
     for i, joint in enumerate(self.joint_list):
       pyrosim.Send_Motor_Neuron(name = sensor_number, jointName = joint)
-      print("name = " + str(sensor_number) + " jointName = " + str(joint))
+      #print("name = " + str(sensor_number) + " jointName = " + str(joint))
       sensor_number = sensor_number + 1
 
       
