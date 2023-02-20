cde28ef19fb333bf5def95001589b0147b52c7c0
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 01:56:23 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -66,7 +66,7 @@ class SOLUTION:
       current = i
       nex = i + 1
       #direction = random.randint(0,1)
-      direction = 1
+      direction = 0
 
       if(random.randrange(0,10) < 5): #sends sensor, makes blue
         pyrosim.Send_Cube(name= str(i), pos=[x+(length/2*width_offset),y,z+height_offset] , size=[length, width, height], color='    <color rgba="0.0 0.0 200.0 1.0"/>', cname = '<material name="Blue">')
