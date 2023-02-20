c06d8b34aeeb6936987ce8c02b775d08fe15df84
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 02:31:23 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -86,7 +86,7 @@ class SOLUTION:
             joint_offset = 1
             pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(length/2*joint_offset),y+(length/2),z+height_offset], jointAxis = "0 1 0")
           else:
-            joint_offset = 2
+            joint_offset = 0
             pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(length/2*joint_offset),y,z+height_offset], jointAxis = "0 1 0")
 
             
@@ -106,7 +106,7 @@ class SOLUTION:
             joint_offset = 1
             pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(length/2),y+(width/2*joint_offset),z+height_offset], jointAxis = "0 1 0")
           else:
-            joint_offset = 2
+            joint_offset = 0
             pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x,y+(width/2*joint_offset),z+height_offset], jointAxis = "0 1 0")
           height_offset = -0.5
           width_offset = 1
