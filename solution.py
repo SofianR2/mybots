fdcd5fdffaa0eedc28b161cfc833957ae8b9e531
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 14:54:58 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -64,7 +64,7 @@ class SOLUTION:
     
     for i in range(self.max):
       self.link_list.append(LINK())
-      print(self.link_list)
+      print(self.link_list.size())
       
     for i, link in enumerate(self.link_list):
       if(i == 0):
