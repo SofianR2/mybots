67cb6088e4d7f5340f398ece1e1f7dd010f6543e
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 13:57:14 -0600

    Update link.py

--- link.py
+++ link.py
@@ -10,9 +10,11 @@ class LINK:
     if(random.randrange(0,10) < 5):
       self.color = '    <color rgba="0.0 0.0 100.0 1.0"/>'
       self.color_name =  '<material name="Blue">'
+      self.has_link = 1
     else:
       self.color = '    <color rgba="0.0 100.0 0.0 1.0"/>'
       self.color_name =  '<material name="Green">'
+      self.has_link = 0
     self.x = random.uniform(0.2, 1)
     self.y = random.uniform(0.2, 1)
     self.z= random.uniform(0.2, 1)
