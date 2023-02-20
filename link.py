7aeaf9bb994595c3bf068d706bc7ea541f6946de
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 13:14:33 -0600

    Create link.py

---
+++ link.py
@@ -0,0 +1,18 @@
+import numpy
+import pyrosim.pyrosim as pyrosim
+import os
+import random
+import time
+import constants as c
+
+class LINK:
+  def __init__(self):
+    if(random.randrange(0,10) < 5):
+      self.color = '    <color rgba="0.0 0.0 100.0 1.0"/>'
+      self.color_name =  '<material name="Blue">'
+    else:
+      self.color = '    <color rgba="0.0 100.0 0.0 1.0"/>'
+      self.color_name =  '<material name="Green">'
+    self.x = random.uniform(0.2, 1)
+    self.y = random.uniform(0.2, 1)
+    self.z= random.uniform(0.2, 1)
