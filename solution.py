cadc968b9d358da01199a86312b6b4356c5205c6
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-20 22:24:59 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -113,7 +113,7 @@ class SOLUTION:
           
         else:
           print("making joint")
-          pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(length/2*joint_offset)-armoffset,y-armoffset/2,z+height_offset], jointAxis = "0 1 0")
+          pyrosim.Send_Joint(name = str(current) + "_" + str(nex), parent= str(current) , child = str(nex) , type = "revolute", position = [x+(length/2*joint_offset)-armoffset,y-armoffset,z+height_offset], jointAxis = "0 1 0")
           armoffset = 0
           height_offset = -0.5
           width_offset = 1
