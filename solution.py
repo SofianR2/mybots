b57d4af0fda19a10d3b280f5a706de0ccfe631a9
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-24 16:07:04 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -219,12 +219,12 @@ class SOLUTION:
     '''
     
 
-    
+    self.num_sensors = numpy.sum(self.get_sensor)
     #for currentRow in range(0, c.numSensorNeurons):##################
     for currentRow in range(0, self.num_sensors):
       for currentColumn in range(0, c.numMotorNeurons):
-        pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + c.numSensorNeurons, weight = self.weights[currentRow][currentColumn])#######
-        #pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + self.num_sensors, weight = self.weights[currentRow-1][currentColumn])
+        #pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + c.numSensorNeurons, weight = self.weights[currentRow][currentColumn])#######
+        pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + self.num_sensors, weight = self.weights[currentRow-1][currentColumn])
     pyrosim.End()
     
   def Evaluate(self, directOrGUI):
