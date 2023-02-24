ae2830257e4c678a2d9bad120c03b560e48badf2
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-24 16:52:38 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -64,7 +64,7 @@ class SOLUTION:
     pyrosim.Start_URDF("body" + str(self.myID) + ".urdf")
     ##############################################
     self.get_sensor = []
-    for i in range(self.max):
+    for j in range(self.max):
       self.link_list.append(LINK())
       #print(len(self.link_list))
       
