17358825b7eefe5f623198733e642695c40d5efa
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-24 23:25:08 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -128,12 +128,12 @@ class SOLUTION:
           jointTrueOffset = length/2 - length
         pyrosim.Send_Joint(name = new_joint_name, parent= str(p), child = str(i), type = "revolute", position = [x + length + jointTrueOffset, y, z + height_offset], jointAxis = "0 1 0")  
         self.joint_list.append(new_joint_name)
-        pyrosim.Send_Cube(name = str(i), pos = [x + length/2, y, z], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
+        height_offset = -0.5
+        pyrosim.Send_Cube(name = str(i), pos = [x + length/2, y, z + height_offset], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
         if(link.color ==  '    <color rgba="0.0 0.0 100.0 1.0"/>'):
           self.get_sensor.append(1)
         else:
           self.get_sensor.append(0)
-        height_offset = -0.5
           
     ##############################################
     '''    
