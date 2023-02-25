48b9b1f5d873faf30f1b8383203ce22bab45b34c
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-25 00:31:16 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -51,7 +51,7 @@ class SOLUTION:
       #move in direction
       pyrosim.Send_Joint(name = new_joint_name, parent= str(p), child = str(i), type = "revolute", position = [x, y, z], jointAxis = "0 1 0")  
       self.joint_list.append(new_joint_name)
-      pyrosim.Send_Cube(name = str(i), pos = [x, y, z], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
+      pyrosim.Send_Cube(name = str(i), pos = [x + length/2, y, z], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
       if(link.color ==  '    <color rgba="0.0 0.0 100.0 1.0"/>'):
         self.get_sensor.append(1)
       else:
