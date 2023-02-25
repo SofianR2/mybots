d411510864778817b16b9bb7462b409e9b5f1bf8
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-25 00:44:29 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -126,8 +126,10 @@ class SOLUTION:
         self.added_links.append(i)
         #make new joint and link
         new_joint_name = str(p) + "_" + str(i)
+        height_offset = 0
         if(i == 1):
           jointTrueOffset = previousx/2 - length
+          height_offset = 1
         pyrosim.Send_Joint(name = new_joint_name, parent= str(p), child = str(i), type = "revolute", position = [x + length + jointTrueOffset, y, z], jointAxis = "0 1 0")  
         self.joint_list.append(new_joint_name)
         height_offset = 0
