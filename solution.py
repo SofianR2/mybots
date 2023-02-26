6d05b29bb65b34cc373d50e6c03d6745c06bdd90
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-26 15:14:18 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -116,9 +116,9 @@ class SOLUTION:
       #initial cube
       if(i == 0):
         pyrosim.Send_Cube(name = str(i), pos = [x, y, z], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
-        print(str(z))
-        print(str(height_offset))
-        print("cube located at: " + str(x) + " " + str(y) + " " + str(z))
+        #print(str(z))
+        #print(str(height_offset))
+        #print("cube located at: " + str(x) + " " + str(y) + " " + str(z))
         #add sensors based on color
         if(link.color ==  '    <color rgba="0.0 0.0 100.0 1.0"/>'):
           self.get_sensor.append(1)
