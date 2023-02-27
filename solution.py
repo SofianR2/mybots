f56c0d98bdf8cffd9d4641b02136bbbd67330447
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-27 15:37:59 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -188,7 +188,7 @@ class SOLUTION:
             z = 0
             pyrosim.Send_Cube(name = str(i), pos = [x+length/2, y, z], size = [link.x, link.y, link.z], color = link.color, cname = link.color_name)
           if(previous_direction == 3):#coming from z
-            pritn("zx")
+            print("zx")
             pyrosim.Send_Joint(name = new_joint_name, parent= str(p), child = str(i), type = "revolute", position = [x+parentx/2 + jointTrueOffset + otherOffset, y, z+parentz/2 - otherOffset3], jointAxis = "0 1 0")  
             self.joint_list.append(new_joint_name)
             z = 0
