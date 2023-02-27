13ce78e9a695d16524d4e0eee3156df97392ac82
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-26 18:18:29 -0600

    Update link.py

--- link.py
+++ link.py
@@ -8,11 +8,11 @@ import constants as c
 class LINK:
   def __init__(self):
     if(random.randrange(0,10) < 5):
-      self.color = '    <color rgba="0.0 0.0 100.0 1.0"/>'
+      self.color = '    <color rgba="0.0 0.0 1.0 1.0"/>'
       self.color_name =  '<material name="Blue">'
       self.has_link = 1
     else:
-      self.color = '    <color rgba="0.0 100.0 0.0 1.0"/>'
+      self.color = '    <color rgba="0.0 1.0 0.0 1.0"/>'
       self.color_name =  '<material name="Green">'
       self.has_link = 0
     self.x = random.uniform(0.2, 1)
