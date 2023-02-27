b5ed1575da2bdd1f1589b77a00debc791443c5a8
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-27 01:07:12 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -154,10 +154,12 @@ class SOLUTION:
         if(direction == 1):#x direction
           if(previous_parent == p):
             previous_direction = 0
-              #print(str(p) + " is already occupied")
-              #print(self.link_list[p].occupied)
-              #previous_direction = 0
-              print("New Previous Direction: " + str(previous_direction))   
+            #print(str(p) + " is already occupied")
+            #print(self.link_list[p].occupied)
+            #previous_direction = 0
+            print("New Previous Direction: " + str(previous_direction))   
+          print("Previous Direction: " + str(previous_direction))
+          print("Direction: " + str(direction))
           if(p == 0):
             jointTrueOffset = parentx/2 - parentx
             otherOffset = parentx/2
@@ -186,6 +188,8 @@ class SOLUTION:
               #print("New Previous Direction: " + str(previous_direction))
           #  otherOffset = parentx/2
           #  otherOffset2 = parenty/2
+          print("Previous Direction: " + str(previous_direction))
+          print("Direction: " + str(direction))
           if(p == 0):
             jointTrueOffset = parenty/2 - parenty
             otherOffset = parentx/2
