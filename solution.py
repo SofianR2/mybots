ae90afbfa8f27ba81089eb355790b261db98fd29
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-27 01:11:19 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -154,7 +154,7 @@ class SOLUTION:
         print("Direction: " + str(direction))
         print("Previous Parent: " + str(previous_parent) + " " + "Current Parent: " + str(p))
         if(direction == 1):#x direction
-          if(previous_parent == p):
+          if(previous_parent == p and p!=0):
             previous_direction = 0
             #print(str(p) + " is already occupied")
             #print(self.link_list[p].occupied)
@@ -181,7 +181,7 @@ class SOLUTION:
           self.SendSensor(link)
           
         if(direction == 2):#y direction
-          if(previous_parent == p):
+          if(previous_parent == p and p!=0):
             previous_direction = 0
               #print(str(p) + " is already occupied")
               #print(self.link_list[p].occupied)
