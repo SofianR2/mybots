0ab74b71eb0c57f5fcb78cbd0e8cae4ce151ff07
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-27 21:05:11 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -56,8 +56,8 @@ class PARALLEL_HILL_CLIMBER:
     #for j in range(c.numberofGenerations):
     #  self.max.append(self.fitness[j][0])
     print(self.fitness)
-    plt.plot(range(1, c.numberOfGenerations), self.fitness)
-    plt.xticks(range(1, c.numberOfGenerations))
+    plt.plot(range(1, c.numberOfGenerations+1), self.fitness)
+    plt.xticks(range(1, c.numberOfGenerations+1))
     plt.xlabel("Generation")
     plt.ylabel("Fitness")
     plt.title("Fitness Evolution")
