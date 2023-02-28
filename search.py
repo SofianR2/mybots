90c340938479fec83dce6932c0df9703b4e96b64
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 15:38:34 -0600

    Update search.py

--- search.py
+++ search.py
@@ -10,6 +10,13 @@ phc.Evolve()
 phc.Show_Best()
 phc.GraphBest()
 
+np.random.seed(2)
+random.seed(2)
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
+phc.GraphBest()
+
 #for x in range(1, 6):
 #  os.system("python3 generate.py")
 #  os.system("python3 simulate.py")
