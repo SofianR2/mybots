a776f0c42ba267f8b4678931ce5cfa0f819cc35e
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 17:24:57 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -25,8 +25,8 @@ class SOLUTION:
     self.link_list = []
     self.added_links = []
     self.joint_list = []
-    #for j in range(self.max):
-    #  self.link_list.append(LINK())
+    for j in range(self.max):
+      self.link_list.append(LINK())
     
     
   def Create_World(self):
@@ -96,9 +96,9 @@ class SOLUTION:
     
     ##############################################
     self.get_sensor = []
-    self.link_list = []
-    for j in range(self.max):
-      self.link_list.append(LINK())
+    #self.link_list = []
+    #for j in range(self.max):
+    #  self.link_list.append(LINK())
       
     for i, l in enumerate(self.link_list):
       l.occupied = [0, 0, 0, 0, 0, 0]
