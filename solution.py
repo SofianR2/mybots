532a5cc88029dc2c359711b023cda55182fa9e6e
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 17:59:06 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -374,7 +374,7 @@ class SOLUTION:
   def Mutate(self):
     #randomRow = random.randint(0, c.numSensorNeurons-1)
     print(self.num_sensors)
-    randomRow = random.randint(0, self.num_sensors-1)
+    randomRow = random.randint(0, self.num_sensors)
     randomColumn = random.randint(0, self.num_motors-1)
     self.weights[randomRow, randomColumn] =  random.random() * 2 - 1
     
