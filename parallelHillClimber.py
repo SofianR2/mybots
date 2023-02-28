8d7f79b0ef321a76139e6d03d73e855a1f636a0b
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 16:59:43 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -16,7 +16,7 @@ class PARALLEL_HILL_CLIMBER:
     self.currentgeneration = 0
     for i in range(0, (c.populationSize)):
       self.parents[i] = SOLUTION(self.nextAvailableID)
-      if(i = 0):
+      if(i == 0):
         self.parents[0].Start_Simulation("GUI")
       self.nextAvailableID += 1
     
