382cd7b5fbc1a078548417c27b490df77a6f630a
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 16:55:06 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -373,6 +373,8 @@ class SOLUTION:
     randomColumn = random.randint(0, self.num_motors-1)
     self.weights[randomRow, randomColumn] =  random.random() * 2 - 1
     
+    self.link_list.append(LINK())
+    
     #random.choice(self.link_list).x += 5
     #self.max += 1
     #c.numSensorNeurons += 1
