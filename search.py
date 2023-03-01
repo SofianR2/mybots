98f78bcec6db885aa9e4c4d48a9d18176e347952
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 21:35:46 -0600

    Update search.py

--- search.py
+++ search.py
@@ -20,7 +20,7 @@ phc.Evolve()
 phc.Show_Best()
 phc.GraphBest()
 graph2 = phc.max
-'''
+
 np.random.seed(3)
 random.seed(3)
 phc = PARALLEL_HILL_CLIMBER()
@@ -62,7 +62,7 @@ for i in range(c.populationSize):
 plt.legend(labels)
 
 plt.show()      
-'''
+
 #for x in range(1, 6):
 #  os.system("python3 generate.py")
 #  os.system("python3 simulate.py")
