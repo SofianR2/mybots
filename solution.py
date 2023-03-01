e0f926187893415398d141d1ac10d4328356a79f
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 18:23:52 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -334,7 +334,7 @@ class SOLUTION:
         #print(self.num_motors)
         #print("AAAAAAAAAAAAAAAA" + str(currentRow))
         #print("CCCCCCCCCCCCCCCCC" + str(currentColumn))
-        pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + self.num_sensors, weight = self.weights[currentRow-3][currentColumn-2])
+        pyrosim.Send_Synapse(sourceNeuronName = currentRow , targetNeuronName = currentColumn + self.num_sensors, weight = self.weights[currentRow-3][currentColumn-3])
     pyrosim.End()
     
   def Evaluate(self, directOrGUI):
