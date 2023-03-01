e6558aa37cf90fef13bf2592895c9f7ab1c1a9e0
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 18:21:33 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -75,8 +75,8 @@ class SOLUTION:
   
   def Create_Body(self):
     #self.get_sensor = []
-    #self.num_sensors = 0
-    #self.num_motors = 0
+    self.num_sensors = 0
+    self.num_motors = 0
     self.coordinates = []
     #self.link_list = []
     self.added_links = []
