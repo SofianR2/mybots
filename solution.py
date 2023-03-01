953463a648671a678cf1a4f74ddf6bd4604c8b69
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-02-28 22:08:46 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -100,7 +100,7 @@ class SOLUTION:
     for j in range(self.max):
       self.link_list.append(LINK())
       if(j == 0):
-        self.link_list[j].x += self.vx
+        #self.link_list[j].x += self.vx
         self.link_list[j].y += self.vy
         #self.link_list[j].z += self.vz
       
