e34382fcef7c32c8b2836d61603dbad7ed020306
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-10 21:19:54 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -64,6 +64,9 @@ class SOLUTION:
     x = self.x
     y = self.y
     z = self.z + height_offset
+    otheroffset = 0
+    otheroffset2 = 0
+    otheroffset3 = 0
 
     #pick random direction to move in
     direction = random.randint(1,3)
