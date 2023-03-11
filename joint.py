21e2f402694b6edeab2714dd8c0c662de239c822
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-10 20:18:18 -0600

    Update joint.py

--- joint.py
+++ joint.py
@@ -6,15 +6,14 @@ import time
 import constants as c
 
 class JOINT:
-  def __init__(self):
-    self.name
-    self.parent
-    self.child
-    self.xpos
-    self.ypos
-    self.zpos
-    self.axis
+  def __init__(self, name, parent, child, x, y, z):
+    self.name = name
+    self.parent = parent
+    self.child = child
+    self.xpos = x
+    self.ypos = y
+    self.zpos = z
   
   def Send_Joint(self):
-    pyrosim.Send_Joint(name = new_joint_name, parent= str(self.parent), child = str(self.child), type = "revolute", position = [self.xpos, self.ypos, self.zpos], jointAxis = "0 1 0")  
+    pyrosim.Send_Joint(name = self.name, parent= str(self.parent), child = str(self.child), type = "revolute", position = [self.xpos, self.ypos, self.zpos], jointAxis = "0 1 0")  
 
