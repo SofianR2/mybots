c1f7751cf77b63fd2bec9aa28680c8565ffd9787
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-13 00:56:22 -0500

    Update search.py

--- search.py
+++ search.py
@@ -15,7 +15,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph1 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(2)
 random.seed(2)
 phc = PARALLEL_HILL_CLIMBER()
@@ -25,7 +25,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph2 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(3)
 random.seed(3)
 phc = PARALLEL_HILL_CLIMBER()
@@ -35,7 +35,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph3 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(4)
 random.seed(4)
 phc = PARALLEL_HILL_CLIMBER()
@@ -45,7 +45,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph4 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(5)
 random.seed(5)
 phc = PARALLEL_HILL_CLIMBER()
@@ -55,7 +55,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph5 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(6)
 random.seed(6)
 phc = PARALLEL_HILL_CLIMBER()
@@ -65,7 +65,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph6 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(7)
 random.seed(7)
 phc = PARALLEL_HILL_CLIMBER()
@@ -75,7 +75,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph7 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(8)
 random.seed(8)
 phc = PARALLEL_HILL_CLIMBER()
@@ -85,7 +85,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph8 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(9)
 random.seed(9)
 phc = PARALLEL_HILL_CLIMBER()
@@ -95,7 +95,7 @@ phc.Show_Best()
 phc.GraphBest()
 graph9 = phc.max
 
-#os.system("del world*.sdf")
+os.system("del world*.sdf")
 np.random.seed(10)
 random.seed(10)
 phc = PARALLEL_HILL_CLIMBER()
