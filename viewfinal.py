90eb3f0580886aafbc5b1255c81bf64af82f103b
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-14 15:29:35 -0500

    Update viewfinal.py

--- viewfinal.py
+++ viewfinal.py
@@ -9,6 +9,42 @@ import time
 
 
 
+with open ("best1.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best2.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
 with open ("best3.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best4.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best5.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best6.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best7.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best8.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best9.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
+
+with open ("best10.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
