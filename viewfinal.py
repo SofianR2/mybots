3ed9838cf594e562cfef13a111f208750966d377
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-14 15:31:15 -0500

    Update viewfinal.py

--- viewfinal.py
+++ viewfinal.py
@@ -17,35 +17,53 @@ f.close()
 with open ("best2.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best3.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best4.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best5.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best6.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best7.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best8.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best9.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
 
 with open ("best10.txt", "rb") as f:
   p = pickle.load(f)
 p[0].Start_Simulation("GUI")
+f.close()
+time.sleep(15)
