ab5c49d60770535de8b98585d37cb6377c32c867
SofianR2 <103147652+SofianR2@users.noreply.github.com>
2023-03-14 15:27:48 -0500

    Update viewfinal.py

--- viewfinal.py
+++ viewfinal.py
@@ -1,1 +1,14 @@
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
+import os
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import constants as c
+import pickle
+import time
 
+
+
+with open ("best1.txt", "rb") as f:
+  p = pickle.load(f)
+p[0].Start_Simulation("GUI")
